45c401291abee1d67ceb9cba3c4f2038d56333ff
Joao Gustavo <jgustavomendonca@gmail.com>
2023-04-06 22:44:23 -0300

    Requisito 2: Implementando a função txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -7,10 +7,15 @@ def txt_importer(path_file):
     if not path_file.endswith(".txt"):
         sys.stderr.write("Formato inválido")
 
-    if path_file == "":
-        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+    # if path_file == "":
+    #     sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
 
-    with open(path_file) as file_txt:
-        lines = file_txt.read()
-        list_txt = lines.split("\n")
-        return list_txt
+    try:
+        with open(path_file) as file_txt:
+            lines = file_txt.read()
+            list_txt = lines.split("\n")
+
+            return list_txt
+
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
