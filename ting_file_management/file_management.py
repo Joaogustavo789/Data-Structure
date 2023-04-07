fb028067f314e3b55843839cabe4419f25a13b7e
Joao Gustavo <jgustavomendonca@gmail.com>
2023-04-06 21:35:03 -0300

    Verificando arquivos que não terminam com .txt

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,11 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    # print(path_file)
+
+    if not path_file.endswith(".txt"):
+        sys.stderr.write("Formato inválido")
+
+    # list_text = list(path_file)
+    # return list_text
