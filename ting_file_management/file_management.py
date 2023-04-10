6b17005b7c85427cabb15acce3ac866185738bd6
Joao Gustavo <jgustavomendonca@gmail.com>
2023-04-10 17:13:59 -0300

    Requisito 3: Implementando a função process

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -2,13 +2,8 @@ import sys
 
 
 def txt_importer(path_file):
-    # print(path_file)
-
     if not path_file.endswith(".txt"):
         sys.stderr.write("Formato inválido")
-
-    # if path_file == "":
-    #     sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
 
     try:
         with open(path_file) as file_txt:
--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,29 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    # print(path_file)
+    # print(instance)
+    for repeat_file in range(len(instance)):
+        if path_file in instance.search(repeat_file).values():
+            return None
+
+    process_file = txt_importer(path_file)
+
+    # print(process_file)
+    line_count = len(process_file)
+
+    new_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": line_count,
+        "linhas_do_arquivo": process_file
+    }
+
+    # print(new_dict)
+
+    instance.enqueue(new_dict)
+    sys.stdout.write(str(new_dict))
 
 
 def remove(instance):
