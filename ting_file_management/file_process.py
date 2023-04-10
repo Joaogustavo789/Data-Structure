144e1521855bd706499bcdf4ecd7ffe108d7118e
Joao Gustavo <jgustavomendonca@gmail.com>
2023-04-10 20:01:13 -0300

    Requisito 4: Implementando a função remove

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -3,15 +3,12 @@ from ting_file_management.file_management import txt_importer
 
 
 def process(path_file, instance):
-    # print(path_file)
-    # print(instance)
     for repeat_file in range(len(instance)):
         if path_file in instance.search(repeat_file).values():
             return None
 
     process_file = txt_importer(path_file)
 
-    # print(process_file)
     line_count = len(process_file)
 
     new_dict = {
@@ -20,14 +17,23 @@ def process(path_file, instance):
         "linhas_do_arquivo": process_file
     }
 
-    # print(new_dict)
-
     instance.enqueue(new_dict)
+
     sys.stdout.write(str(new_dict))
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    # print(instance)
+
+    if instance.__len__() == 0:
+        sys.stdout.write("Não há elementos\n")
+
+    else:
+        path_file = instance.dequeue()
+
+        file_path = path_file['nome_do_arquivo']
+
+        sys.stdout.write(f"Arquivo {file_path} removido com sucesso\n")
 
 
 def file_metadata(instance, position):
