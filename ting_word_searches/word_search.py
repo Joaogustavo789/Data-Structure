72f8fe63e6a3fb7f7fd5562d85eaf5f0587f524c
Joao Gustavo <jgustavomendonca@gmail.com>
2023-04-11 19:45:33 -0300

    Palavra não encontrada, retorna lista vazia

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,27 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação!"""
+    print(instance)
+
+    for find_word in range(len(instance)):
+        if word not in instance.search(find_word).values():
+            return []
+
+    new_list = [
+        {
+            "palavra": word,
+            "arquivo": "statics/nome_pedro.txt",
+            "ocorrencias": [
+                {
+                    "linha": ""
+                },
+
+                {
+                    "linha": ""
+                }
+            ]
+        }
+    ]
+
+    return new_list
 
 
 def search_by_word(word, instance):
