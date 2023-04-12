9a9496118850baf8a38358a654387a20a9b59ea8
Joao Gustavo <jgustavomendonca@gmail.com>
2023-04-12 18:48:19 -0300

    Requisito 8: Implementando a função search_by_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,4 @@
 def exists_word(word, instance):
-    # print(instance.showQueue())
-
     new_list = []
 
     for file in instance.showQueue():
@@ -27,4 +25,27 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    new_list2 = []
+
+    for file2 in instance.showQueue():
+        ocorrencias2 = []
+
+        for index2, line2 in enumerate(file2["linhas_do_arquivo"]):
+            if word.lower() in line2.lower():
+                ocorrencias2.append(
+                    {
+                        "linha": index2 + 1,
+                        "conteudo": line2
+                    },
+                )
+
+        if ocorrencias2:
+            exist_word_list2 = {
+                "palavra": word,
+                "arquivo": file2["nome_do_arquivo"],
+                "ocorrencias": ocorrencias2
+            }
+
+            new_list2.append(exist_word_list2)
+
+    return new_list2
